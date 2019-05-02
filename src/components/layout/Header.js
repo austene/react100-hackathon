24b0c2e45bac8aa780898311743f4d6d32d967fd
AE <austence@sbcglobal.net>
2019-05-01 22:27:29 -0700

    corrects header mx margin.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header 
-      className='jumbotron p-2 mx-5 my-3 display-3 bg-white text-center border border-black'>
+      className='jumbotron p-2 mx-4 my-3 display-3 bg-white text-center border border-black'>
       <h1>Today's Asteroids</h1>
     </header>
   )
